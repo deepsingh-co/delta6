b0294fc99c81c588c3557946ff4ac6e05e5ae179
deepsingh-co <deepsinghanirudh1@gmail.com>
2025-07-05 15:37:15 +0530

    object

---
+++ objectjs/pratice.js
@@ -0,0 +1,17 @@
+const dics = Math.floor(Math.random()*6)+1;
+
+const car ={
+    nmae: "maruti",
+    Model: "856js",
+    color: "black",
+};
+
+const person ={
+    name : "Anurudh",
+    age: 25,
+    city: "mumbai",
+};
+person.city ="new york";
+person.country ="london";
+
+console.log(person);
