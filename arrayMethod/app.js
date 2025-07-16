c5602faee01d83f786e6e283fdc0324eddf4f4e2
deepsingh-co <deepsinghanirudh1@gmail.com>
2025-07-16 20:36:06 +0530

    arrnetgod

---
+++ arrayMethod/app.js
@@ -0,0 +1,36 @@
+//ARRAY METHOD
+
+let arr =[1,2,3,4,5];
+
+arr.forEach(function(el){
+    console.log(el);
+});
+
+let print = function(el){
+    console.log(el);
+};
+
+arr.forEach(print);
+
+let arrs = [{
+    name:"Anurudh",
+    age:21,
+},{
+    name: "Komal",
+    age:20,
+},{
+    name:"Anshika",
+    age:19,
+},];
+
+arrs.forEach((student) =>{
+    console.log(student.age);
+},);
+
+//MAP AND FILTER
+
+let num = [1,2,3,4];
+
+let double = num.map((el) =>{
+    return el *el;
+});
