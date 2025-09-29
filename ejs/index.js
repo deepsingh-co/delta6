3f80e4a41d0f1dff1200c8b65809e61993a118e9
deepsingh-co <deepsinghanirudh1@gmail.com>
2025-09-29 22:18:56 +0530

    ejs

--- ejs/index.js
+++ ejs/index.js
@@ -6,14 +6,32 @@ const port = 8080;
 
 app.set("view engine" , "ejs");
 
-app.set("views" , Path2D.join(__dirname,"views"));
+app.set("views" , path.join(__dirname,"/views"));
+
 app.get("/", (req , res)=>{
     res.render("home.ejs");
+});
+
+app.get("/rolldics" , (req , res) => {
+    let diceVal = Math.floor(Math.random() * 6) +1;
+    res.render("rolldics.ejs" , {diceVal});
+});
+
+
+
+
+app.get("/ig/:username" , (req , res) =>{
+    let{username} = req.params;
+    res.render("instagram.ejs" , {username})
 })
+
+
 
 app.listen(port , () =>{
     console.log(`Listening on port ${port}`)
 });
-app.listen(port , () =>{
-    console.log(`Listening on port ${port}`)
-});
+//app.listen(port , () =>{
+ //   console.log(`Listening on port ${port}`)
+//});
+
+
